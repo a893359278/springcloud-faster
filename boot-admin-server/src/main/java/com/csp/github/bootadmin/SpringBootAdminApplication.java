b0ae14212cc3960ef4074859d5ac64c24610bb6f
chenshaoping <18250073990@163.com>
2020-01-29 11:27:46 +0800

    去除 spring-boot-admin-client-starter

--- boot-admin-server/src/main/java/com/csp/github/bootadmin/SpringBootAdminApplication.java
+++ boot-admin-server/src/main/java/com/csp/github/bootadmin/SpringBootAdminApplication.java
@@ -1,7 +1,6 @@
 package com.csp.github.bootadmin;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -17,7 +16,6 @@ public class SpringBootAdminApplication {
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(SpringBootAdminApplication.class)
-                .web(WebApplicationType.REACTIVE)
                 .run(args);
     }
 }
