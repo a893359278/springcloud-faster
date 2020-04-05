b8906c2a75d41fc6039748b2c30291dc1b6f863f
chenshaoping <18250073990@163.com>
2020-04-05 15:17:03 +0800

    增强异常处理

--- parent/web/web-common/src/main/java/com/csp/github/base/web/exception/DefaultGlobalExceptionHandlerAdvice.java
+++ parent/web/web-common/src/main/java/com/csp/github/base/web/exception/DefaultGlobalExceptionHandlerAdvice.java
@@ -1,13 +1,16 @@
 package com.csp.github.base.web.exception;
 
 
+import com.alibaba.fastjson.JSON;
 import com.csp.github.base.common.entity.DefaultResultType;
 import com.csp.github.base.common.entity.Result;
 import com.csp.github.base.common.exception.BaseException;
 import com.csp.github.base.common.exception.ServiceException;
 import com.csp.github.base.common.exception.ServiceSpecialException;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
@@ -19,6 +22,8 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 /**
  * @author 陈少平
@@ -28,9 +33,23 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class DefaultGlobalExceptionHandlerAdvice {
 
+    private void logExceptionInfo(Throwable e) {
+        HttpServletRequest request = ((ServletRequestAttributes) (RequestContextHolder.currentRequestAttributes())).getRequest();
+        String method = request.getMethod();
+        String uri = request.getRequestURI();
+        Map<String, String[]> requestParameterMap = request.getParameterMap();
+        log.error("请求路径[{}],请求方式[{}],请求入参[{}]", uri, method, JSON.toJSONString(requestParameterMap), e);
+    }
+
+    @ExceptionHandler(value = {Throwable.class})
+    public Result exception(Throwable e) {
+        logExceptionInfo(e);
+        return Result.fail();
+    }
+
     @ExceptionHandler(value = {NullPointerException.class})
     public Result nullPointException(NullPointerException e) {
-        ExceptionHelper.logException(e);
+        logExceptionInfo(e);
         return Result.fail("数据走丢啦");
     }
 
@@ -39,19 +58,19 @@ public class DefaultGlobalExceptionHandlerAdvice {
      */
     @ExceptionHandler(value = {DuplicateKeyException.class})
     public Result duplicateKeyException(DuplicateKeyException ex) {
-        ExceptionHelper.logException(ex);
+        logExceptionInfo(ex);
         return Result.fail(DefaultResultType.DUPLICATE_PRIMARY_KEY);
     }
 
     @ExceptionHandler(value = {BaseException.class, ServiceException.class})
     public Result serviceException(BaseException ex) {
-        ExceptionHelper.logException(ex);
+        logExceptionInfo(ex);
         return Result.fail(ex);
     }
 
     @ExceptionHandler(value = {ServiceSpecialException.class})
     public Result serviceSpecialException(ServiceSpecialException ex) {
-        log.error("业务异常:{}, 携带数据:{}", ex.getMsg(), ex.getData());
+        logExceptionInfo(ex);
         return Result.fail(ex);
     }
 
@@ -61,7 +80,7 @@ public class DefaultGlobalExceptionHandlerAdvice {
      */
     @ExceptionHandler(NumberFormatException.class)
     public Object numberFormatException(NumberFormatException ex) {
-        ExceptionHelper.logException(ex);
+        logExceptionInfo(ex);
         return Result.fail(DefaultResultType.FORMAT_EXCEPTION.getCode(), DefaultResultType.FORMAT_EXCEPTION.getMsg());
     }
 
@@ -81,7 +100,7 @@ public class DefaultGlobalExceptionHandlerAdvice {
             sb.append(violation.getMessage());
         }
         String msg = sb.toString();
-        log.error("自定义参数校验失败:{}", msg);
+        logExceptionInfo(ex);
         return Result.fail(DefaultResultType.PARAM_INVALID.getCode(), msg);
     }
 
