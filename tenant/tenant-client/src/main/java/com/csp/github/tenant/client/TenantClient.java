fc2b16a87519ef5a644c21a0ff3fc71b51777704
chenshaoping <18250073990@163.com>
2020-06-07 23:20:49 +0800

    最近在准备面试，没啥时间。今天稍微看了一眼，发现好的 隐藏的 bug。。。之后再来好好重新写一下

--- tenant/tenant-client/src/main/java/com/csp/github/tenant/client/TenantClient.java
+++ tenant/tenant-client/src/main/java/com/csp/github/tenant/client/TenantClient.java
@@ -8,17 +8,17 @@ package com.csp.github.tenant.client;
 import com.csp.github.tenant.entity.Tenant;
 import com.csp.github.tenant.entity.TenantPermission;
 import java.util.List;
-import javax.validation.constraints.NotNull;
 import org.springframework.cloud.openfeign.DefaultHystrixFallbackFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(value = "tenant-service", path = "/tenant", fallbackFactory = DefaultHystrixFallbackFactory.class)
 public interface TenantClient {
 
     @GetMapping("/tenant/username")
-    Tenant getTenantByUsername(@NotNull String username);
+    Tenant getTenantByUsername(@RequestParam String username);
 
     @GetMapping(value = "/permission")
-    List<TenantPermission> getPermissionList(@NotNull Long tenantId);
+    List<TenantPermission> getPermissionList(@RequestParam Long tenantId);
 }
